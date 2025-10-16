7a32c72b4afbf176bdf4d91408d2182f77e8b516
pablo <pablo.carbayo@alu.uclm.es>
2025-10-16 15:58:17 +0200

    generado nuevo backend

---
+++ gramolabe/src/main/java/com/gramlolabe/App.java
@@ -0,0 +1,13 @@
+package com.gramlolabe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
