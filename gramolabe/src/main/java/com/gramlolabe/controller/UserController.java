9f48746312faa4382f33808b263f519da94196ab
pablo <pablo.carbayo@alu.uclm.es>
2025-10-18 17:39:27 +0200

    feat: Actualizar configuración de Maven y permitir CORS en UserController

--- gramolabe/src/main/java/com/gramlolabe/controller/UserController.java
+++ gramolabe/src/main/java/com/gramlolabe/controller/UserController.java
@@ -3,6 +3,7 @@ package com.gramlolabe.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gramlolabe.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // Permite peticiones desde el frontend
 public class UserController {
     @Autowired
     private UserService service;
