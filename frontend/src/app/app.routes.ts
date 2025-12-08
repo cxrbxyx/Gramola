3bf8e848ad4ed0f1714b98d879bee5ad129d3737
Pablo <pablo.carbayo@alu.uclm.es>
2025-12-08 01:05:34 +0100

    Agregar componente Home con su HTML, CSS y pruebas; actualizar rutas para redirigir a Home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register';
 import { PaymentsComponent } from './payments/payments';
 import { Callback } from './callback/callback';
+import { Home } from './home/home';
+//import { LoginComponent } from './login/login';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: '', component: Home },
   { path: 'payments', component: PaymentsComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'callback', component: Callback }
+  { path: 'callback', component: Callback },
+  //{ path: 'login', component: LoginComponent }
 ];
